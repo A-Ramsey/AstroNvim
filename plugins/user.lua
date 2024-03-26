6caea40caaddaa949e619787f48574a2a77e5671
Aaron Ramsey <aaronramsey2000@gmail.com>
2024-03-26 20:49:30 +0000

    feat: change theme to catppuccin

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,4 +1,6 @@
 return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.colorscheme.catppuccin" }
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
